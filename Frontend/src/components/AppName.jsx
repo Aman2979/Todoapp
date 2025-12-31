be2575229964b385df192e066e52a90e98e92eff
Aman Kumar <ak93896897@gmail.com>
2025-12-31 17:02:51 +0530

    Test

--- Frontend/src/components/AppName.jsx
+++ Frontend/src/components/AppName.jsx
@@ -2,7 +2,7 @@ const AppName = () => {
   return (
     <header className="w-full py-8 bg-blue-300 shadow-lg mb-8">
       <h1 className="text-4xl sm:text-5xl font-extrabold text-white tracking-tight text-center drop-shadow-lg">
-        <span className="bg-white/10 px-4 py-2 rounded-xl">Todo App</span>
+        <span className="bg-white/10 px-4 py-2 rounded-xl">Todo Ap</span>
       </h1>
       <p className="text-center text-base sm:text-lg mt-6 text-white/90 font-medium">
         Organize your day efficiently and stylishly!
