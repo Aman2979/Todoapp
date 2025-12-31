5b837b21c73798209ce093d0226a42d7b5ede419
Aman Kumar <ak93896897@gmail.com>
2025-12-31 17:11:15 +0530

    Change date

--- Frontend/src/components/AddTodo.jsx
+++ Frontend/src/components/AddTodo.jsx
@@ -40,6 +40,7 @@ const AddTodo = () => {
       />
       <input
         type="date"
+        placeholder="Enetr future dates.."
         ref={todoDateInput}
         className="px-3 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-400 focus:outline-none text-base transition"
       />
