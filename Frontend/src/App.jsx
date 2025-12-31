14cc987b030a6e6046319d37a7f857bda9f6003a
Aman Kumar <ak93896897@gmail.com>
2025-12-31 17:15:16 +0530

    Add footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import AddTodo from "./components/AddTodo";
 import AppName from "./components/AppName";
+import Footer from "./components/Footer";
 import LoadItems from "./components/LoadItems";
 import TodoItems from "./components/TodoItems";
 import { TodoItemsProvider } from "./store/TodoItemsContext";
@@ -13,6 +14,7 @@ function App() {
         <LoadItems />
         <TodoItems />
       </center>
+      <Footer />
     </TodoItemsProvider>
   );
 }
---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="w-full mt-16 py-4 border-t border-gray-200 text-center text-sm text-gray-500">
+      © 2025 Todo App · Built with React
+    </footer>
+  );
+};
+
+export default Footer;
